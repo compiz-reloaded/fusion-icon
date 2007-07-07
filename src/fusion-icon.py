e7a5c0a35998318126872324182943e3afae53c0
Christopher Williams <christopherw@verizon.net>
2007-07-07 12:24:21 -0400

    Minor fixes.

--- src/fusion-icon.py
+++ src/fusion-icon.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # Compiz Fusion Icon
-
-import os, sys, time
+from os import system, environ, path, rename
+from sys import exit, argv
+from time import time
 
 def help():
 	print 'Usage: fusion-icon [action|interface]'
@@ -13,7 +14,7 @@ def help():
 	print '  --gtk      Use the pygtk 2.10 interface'
 	print '  --qt3      Use the PyQt3 interface (currently not installed by default)'
 	print '  --qt4      Use the PyQt4 interface'
-	sys.exit(0)
+	exit(0)
 
 def reset():
 	#cut-and-pasted from libfusionicon to avoid a full load for --reset 
@@ -22,49 +23,53 @@ def reset():
 	print '* Configuration file (' + config_file + ') being reset'
 	try:
 		if path.exists(config_file):
-			config_backup = path.join(config_folder, 'fusion-icon.backup.' + str(int(time.time())))
+			config_backup = path.join(config_folder, 'fusion-icon.backup.' + str(int(time())))
 			rename(config_file, config_backup)
 			print '... backed up to:', config_backup
+		else:
+			print '... no configuration found'
 		print '* Configuration reset'
-		sys.exit(0)
-		
+			
 	except:
 		print '* Error: configuration reset failed'
+		exit(1)
 		
+	exit(0)
+	
 interfaces={'gtk':('GTK', ('pygtk', 'interface_gtk')),
 	'qt4':('Qt4', ('PyQt4', 'interface_qt4')),
-	'qt3':('Qt3', ('qt', 'ctypes', 'interface_qt3'))}
+	'qt3':('Qt3', ('qt','ctypes', 'interface_qt3'))}
 
 def int_import(interface):
 	try:
 		print '* Using the ' + interfaces[interface][0] + ' Interface'
-		for x in interfaces[interface][1]:
-			exec('import ' + x)
+		for module in interfaces[interface][1]:
+			exec('import ' + module)
 			
 	except ImportError:
-		print '* Error: failed to import: ' + "".join(interfaces[interface][1])
-		sys.exit(1)
+		print '* Error: failed to import: ' + ', '.join(interfaces[interface][1])
+		exit(1)
 
 ## Detect and run args
 # If we're running --help or --reset, don't progress past it
-if '--help' in sys.argv or '-h' in sys.argv	:
+if '--help' in argv or '-h' in argv	:
 	help()
 
-if '--reset' in sys.argv:
+if '--reset' in argv:
 	reset()
 	
 # Remove the need to import libfusionicon.py
 # We'll detect kde right here
 kde = False
-if os.system('pgrep kdesktop 1>/dev/null') == 0 or os.system('pgrep startkde 1>/dev/null') == 0:
+if system('pgrep kdeinit 1>/dev/null') == 0 or system('pgrep startkde 1>/dev/null') == 0:
 	kde = True
 
 # Use what's specified, or use autodetection
-if '--qt3' in sys.argv:
+if '--qt3' in argv:
 	int_import('qt3')
 
 else:
-	if (kde and not '--gtk' in sys.argv) or '--qt4' in sys.argv:
+	if (kde and not '--gtk' in argv) or '--qt4' in argv:
 		int_import('qt4')	
 	
 	else:
