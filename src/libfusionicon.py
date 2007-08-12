b91e1d8e852fe1fa8c4bb33505c305652caea7e5
Christopher Williams <christopherw@verizon.net>
2007-08-12 18:49:41 -0400

    Add archlinux mesa libgl

--- src/libfusionicon.py
+++ src/libfusionicon.py
@@ -18,7 +18,13 @@
 import ConfigParser, compizconfig, commands, os, subprocess, time
 
 # Define variables
-mesa_libgl_locations = ('/usr/lib/fglrx/libGL.so.1.2.xlibmesa', '/opt/mesa-xgl/lib/libGL.so.1.2')
+mesa_libgl_locations = (
+	'/usr/lib/fglrx/libGL.so.1.2.xlibmesa',
+	'/opt/mesa-xgl/lib/libGL.so.1.2',
+	'/lib/mesa/libGL.so.1.2',
+	'/usr/lib/opengl/xorg-x11/lib/libGL.so.1.2',
+)
+
 tfp = 'GLX_EXT_texture_from_pixmap'
 
 apps = ('compiz.real', 'ccsm', 'compiz', 'gtk-window-decorator', 'kde-window-decorator', 'emerald', 'metacity', 'kwin', 'xfwm4') 
