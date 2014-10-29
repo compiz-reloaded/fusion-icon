6e5a3cfba83db4f2a1025465254e49647d8b9680
kozec <kozec@kozec.com>
2014-10-29 21:54:16 +0100

    Gtk3 support

--- FusionIcon/interface_gtk/main.py
+++ FusionIcon/interface_gtk/main.py
@@ -16,50 +16,45 @@
 #
 # Based on compiz-icon, Copyright 2007 Felix Bellanger <keeguon@gmail.com>
 #
-# Author(s): crdlb
-# Copyright 2007 Christopher Williams <christopherw@verizon.net> 
+# Original author: crdlb
+# GTK3 port: kozec
 
-import os
-import gtk
-if gtk.pygtk_version < (2,10,0):
-	# raise an ImportError here to trigger the Except statement in interface.py
-    raise ImportError, 'PyGtk 2.10.0 or later required'
-
-import time
+import os, time
+from gi.repository import Gtk
 from FusionIcon.start import wms, apps, options, decorators, init
 
-class TrayMenu(gtk.Menu):
+class TrayMenu(Gtk.Menu):
 	
 	def __init__(self):
-		gtk.Menu.__init__(self)
+		Gtk.Menu.__init__(self)
 
 		#CCSM
 		if 'ccsm' in apps:
 			item = ApplicationItem('ccsm')
-			item.set_image(gtk.image_new_from_stock('gtk-preferences', gtk.ICON_SIZE_MENU))
+			item.set_image(Gtk.Image.new_from_stock('gtk-preferences', Gtk.IconSize.MENU))
 			self.append(item)
 
 		#Emerald Theme Manager
 		if 'emerald theme manager' in apps:
 			item = ApplicationItem('emerald theme manager')
-			item.set_image(gtk.image_new_from_icon_name('emerald-theme-manager-icon', gtk.ICON_SIZE_MENU))
+			item.set_image(Gtk.Image.new_from_icon_name('emerald-theme-manager-icon', Gtk.IconSize.MENU))
 			self.append(item)
 
 		if 'ccsm' in apps or 'emerald theme manager' in apps:
-			item = gtk.SeparatorMenuItem()
+			item = Gtk.SeparatorMenuItem()
 			self.append(item)
 
 		#Reload
-		item = gtk.ImageMenuItem('Reload Window Manager')
+		item = Gtk.ImageMenuItem('Reload Window Manager')
 		item.connect('activate', self.reload_activate)
-		item.set_image(gtk.image_new_from_stock('gtk-refresh', gtk.ICON_SIZE_MENU))
+		item.set_image(Gtk.Image.new_from_stock('gtk-refresh', Gtk.IconSize.MENU))
 		if not wms:
 			item.set_sensitive(False)
 		self.append(item)
 
 		#Window Manager
-		item = gtk.ImageMenuItem('Select Window Manager')
-		item.set_image(gtk.image_new_from_stock('gtk-index', gtk.ICON_SIZE_MENU))
+		item = Gtk.ImageMenuItem('Select Window Manager')
+		item.set_image(Gtk.Image.new_from_stock('gtk-index', Gtk.IconSize.MENU))
 		submenu = WindowManagerMenu()
 		item.set_submenu(submenu)
 		if not wms:
@@ -67,8 +62,8 @@ class TrayMenu(gtk.Menu):
 		self.append(item)
 
 		#Compiz Options
-		item = gtk.ImageMenuItem('Compiz Options')
-		item.set_image(gtk.image_new_from_stock('gtk-properties', gtk.ICON_SIZE_MENU))
+		item = Gtk.ImageMenuItem('Compiz Options')
+		item.set_image(Gtk.Image.new_from_stock('gtk-properties', Gtk.IconSize.MENU))
 		submenu = CompizOptionMenu()
 		item.set_submenu(submenu)
 		if not options:
@@ -76,35 +71,35 @@ class TrayMenu(gtk.Menu):
 		self.append(item)
 
 		#Window Decorator
-		item = gtk.ImageMenuItem('Select Window Decorator')
-		item.set_image(gtk.image_new_from_stock('gtk-select-color', gtk.ICON_SIZE_MENU))
+		item = Gtk.ImageMenuItem('Select Window Decorator')
+		item.set_image(Gtk.Image.new_from_stock('gtk-select-color', Gtk.IconSize.MENU))
 		submenu = CompizDecoratorMenu()
 		item.set_submenu(submenu)
 		if not decorators:
 			item.set_sensitive(False)
 		self.append(item)
 
-		item = gtk.SeparatorMenuItem()
+		item = Gtk.SeparatorMenuItem()
 		self.append(item)
 
-		item = gtk.ImageMenuItem(stock_id=gtk.STOCK_QUIT)
+		item = Gtk.ImageMenuItem.new_from_stock(Gtk.STOCK_QUIT)
 		item.connect('activate', self.quit_activate)
 		self.append(item)
 
 	def show_menu(self, widget, button, time):
 		self.show_all()
-		self.popup(None, None, gtk.status_icon_position_menu, button, time, icon)
+		self.popup(None, None, None, Gtk.StatusIcon.position_menu, button, time)
 
 	def reload_activate(self, widget):
 		wms.restart()
 
 	def quit_activate(self, widget):
-		gtk.main_quit()
+		Gtk.main_quit()
 
-class ApplicationItem(gtk.ImageMenuItem):
+class ApplicationItem(Gtk.ImageMenuItem):
 
 	def __init__(self, app):
-		gtk.ImageMenuItem.__init__(self, apps[app].label)
+		Gtk.ImageMenuItem.__init__(self, apps[app].label)
 
 		self.app = app
 		if app not in apps:
@@ -114,14 +109,12 @@ class ApplicationItem(gtk.ImageMenuItem):
 	def activate(self, widget):
 		apps[self.app].launch()
 
-class WindowManagerItem(gtk.RadioMenuItem):
+class WindowManagerItem(Gtk.RadioMenuItem):
 
-	def __init__(self, wm, first_item=None):
-		gtk.RadioMenuItem.__init__(self, label=' %s' %wms[wm].label)
+	def __init__(self, wm, group):
+		Gtk.RadioMenuItem.__init__(self, label=' %s' %wms[wm].label, group=group)
 
 		self.wm = wm
-		if first_item:
-			self.set_group(first_item)
 		if wms.active == wm:
 			self.set_active(True)
 		self.connect('activate', self.activate)
@@ -131,25 +124,21 @@ class WindowManagerItem(gtk.RadioMenuItem):
 			wms.active = self.wm
 			wms.start()
 
-class WindowManagerMenu(gtk.Menu):
+class WindowManagerMenu(Gtk.Menu):
 
 	def __init__(self):
-		gtk.Menu.__init__(self)
+		Gtk.Menu.__init__(self)
 
-		first = True
+		prev = None
 		for wm in wms.ordered_list:
-			if first:
-				first_item = WindowManagerItem(wm)
-				self.append(first_item)
-				first = False
-			else:
-				item = WindowManagerItem(wm, first_item)
-				self.append(item)
+			item = WindowManagerItem(wm, prev)
+			self.append(item)
+			prev = item
 
-class CompizOptionItem(gtk.CheckMenuItem):
+class CompizOptionItem(Gtk.CheckMenuItem):
 
 	def __init__(self, option):
-		gtk.CheckMenuItem.__init__(self, label=' %s' %options[option].label)
+		Gtk.CheckMenuItem.__init__(self, label=' %s' %options[option].label)
 
 		self.option = option
 		self.set_active(options[option].enabled)
@@ -162,23 +151,21 @@ class CompizOptionItem(gtk.CheckMenuItem):
 		if wms.active == 'compiz':
 			wms.start()
 
-class CompizOptionMenu(gtk.Menu):
+class CompizOptionMenu(Gtk.Menu):
 
 	def __init__(self):
-		gtk.Menu.__init__(self)
+		Gtk.Menu.__init__(self)
 
 		for option in options:
 			item = CompizOptionItem(option)
 			self.append(item)
 
-class CompizDecoratorItem(gtk.RadioMenuItem):
+class CompizDecoratorItem(Gtk.RadioMenuItem):
 
-	def __init__(self, decorator, first_item=None):
-		gtk.RadioMenuItem.__init__(self, label=' %s' %decorators[decorator].label)
+	def __init__(self, decorator, group):
+		Gtk.RadioMenuItem.__init__(self, label=' %s' %decorators[decorator].label, group=group)
 
 		self.decorator = decorator
-		if first_item:
-			self.set_group(first_item)
 		if decorators.active == decorator:
 			self.set_active(True)
 		self.connect('activate', self.activate)
@@ -189,27 +176,24 @@ class CompizDecoratorItem(gtk.RadioMenuItem):
 			time.sleep(0.5)
 			decorators.active = self.decorator
 
-class CompizDecoratorMenu(gtk.Menu):
+class CompizDecoratorMenu(Gtk.Menu):
 
 	def __init__(self):
-		gtk.Menu.__init__(self)
+		Gtk.Menu.__init__(self)
 
-		first = True
+		prev = None
 		for decorator in decorators:
-			if first:
-				first_item = CompizDecoratorItem(decorator)
-				self.append(first_item)
-				first = False
-			else:
-				item = CompizDecoratorItem(decorator, first_item)
-				self.append(item)
+			item = CompizDecoratorItem(decorator, prev)
+			item.set_name(str(decorator))
+			print "I", item.get_group()
+			self.append(item)
+			prev = item
 
-icon = gtk.status_icon_new_from_icon_name('fusion-icon')
-icon.set_tooltip('Compiz Fusion Icon')
+icon = Gtk.StatusIcon.new_from_icon_name('fusion-icon')
+icon.set_tooltip_text('Compiz Fusion Icon')
 menu = TrayMenu()
 icon.connect('popup-menu', menu.show_menu)
 
 # active wm (possibly) starts here
 init()
-gtk.main()
-
+Gtk.main()
